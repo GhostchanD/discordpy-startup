9e73cb5adb64f77b068553c88210cb8acecbce58
GhostchanD <62089559+GhostchanD@users.noreply.github.com>
2020-03-13 01:49:24 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -6,17 +6,15 @@ bot = commands.Bot(command_prefix='+')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
-@client.event()
+@client.event
+async def on_ready():
+  print("logged in as " + client.user.name)
+
+@client.event
 async def on_message(message):
-    if message.author != client.user:
-        msg = message.author.mention + " HEY "
-        await client.send_message(message.channel, msg)
-        
-@bot.command()
-async def test(ctx):
-    await ctx.send('test world 1')
-    await ctx.send('test world 2')
-    await ctx.send('test world 3')
-    await ctx.send('test world 4')
+  if message.author != client.user:
+    msg = message.author.mention + " Hi."
+    await client.send_message(message.channel, msg)
+
     
 bot.run(token)
